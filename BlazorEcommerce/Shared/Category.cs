d3606238cb09abd5a8e43d1b9df8aacc4fa1c5f7
angela <angela_yk@hotmail.com>
2022-09-08 17:57:49 +1200

    Added more products to database

---
+++ BlazorEcommerce/Shared/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazorEcommerce.Shared
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+    }
+}
+
