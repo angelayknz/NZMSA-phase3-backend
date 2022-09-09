69e79667267a85898d122cee609d8595b180e2e5
angela <angela_yk@hotmail.com>
2022-09-09 22:31:16 +1200

    Update content

---
+++ UnitTest/CouterTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Bunit;
+using BlazorEcommerce.Client.Pages;
+using BlazorEcommerce.Client.Shared;
+
+namespace BlazorAppTests
+{
+    public class CounterTest
+    {
+        [Fact]
+        public void RendersSuccessfully()
+        {
+
+            using var ctx = new TestContext();
+
+            // Render Counter component.
+            var component = ctx.RenderComponent<Counter>();
+
+            // Assert: first, find the parent_name vital element, then verify its content.
+            Assert.Equal("Click me", component.Find($".btn").TextContent);
+        }
+
+    }
+}
