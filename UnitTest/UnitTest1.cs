ca225b7ec3b5468f17d4cf1427b6e58a254badfc
angela <angela_yk@hotmail.com>
2022-09-11 09:40:40 +1200

    Add unit test

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Bunit;
+using Xunit;
+using BlazorEcommerce.Client.Pages;
+
+namespace BlazorAppTests
+{
+    public class CounterTest
+    {
+        [Fact]
+        public void RendersSuccessfully()
+        {
+
+            using var ctx = new TestContext();
+
+            // Render Counter component.
+            var component = ctx.RenderComponent<Counter>();
+
+            // Assert: first, find the parent_name vital element, then verify its content.
+            Assert.Equal("Click me", component.Find($".btn").TextContent);
+        }
+
+    }
+}
